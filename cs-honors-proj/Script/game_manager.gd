3f41c95f32c8704ccb2fca301b5322f938220c4c
qc33 <qc33@illinois.edu>
2025-10-14 03:36:05 +0800

    music, sounds, creating coins each 10s when interact with trash bin, and no repeated coins creating

--- cs-honors-proj/Script/game_manager.gd
+++ cs-honors-proj/Script/game_manager.gd
@@ -1,6 +1,10 @@
 extends Node
 @onready var score_label: Label = $ScoreLabel
 var score = 0
+func _ready():
+	#connect autoload GameManager to find ScoreLabel
+	score_label = get_tree().get_root().find_child("ScoreLabel", true, false)
 func add_point():
 	score += 1
-	score_label.text = "Coins: " + str(score)
+	if score_label:
+		score_label.text = "Coins: " + str(score)
