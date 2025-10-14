1315c7f60080e71d3272583301ab9d45eac7fe92
qc33 <qc33@illinois.edu>
2025-10-15 05:24:43 +0800

    add npcs and interact with npcs to collect coins

--- cs-honors-proj/Script/game_manager.gd
+++ cs-honors-proj/Script/game_manager.gd
@@ -1,4 +1,6 @@
 extends Node
+
+#part for score(coin) collecting
 @onready var score_label: Label = $ScoreLabel
 var score = 0
 func _ready():
@@ -8,3 +10,28 @@ func add_point():
 	score += 1
 	if score_label:
 		score_label.text = "Coins: " + str(score)
+
+# part for npc creating
+@export var human_copy = preload("res://Scene/human.tscn")
+@export var creat_cd := 3.0  # create humann each 3 seconds
+@export var left_create_x := -550   # create outside left killzone
+@export var right_create_x := 600   # create outside right killzone
+@export var create_y := 80   # create on ground
+
+var time_count = 0.0
+func create_human():
+	var human = human_copy.instantiate() # create new human scene
+	var random_side = randi() % 2 # create random 0 or 1
+	if random_side == 0 :
+		human.position = Vector2(left_create_x, create_y);
+		human.direction = 1;
+	else:
+		human.position = Vector2(right_create_x, create_y);
+		human.direction = -1;
+	add_child(human) #add our new human
+	
+func _process(delta):
+	time_count += delta
+	if time_count >= creat_cd:
+		time_count = 0
+		create_human()
--- cs-honors-proj/Script/human.gd
+++ cs-honors-proj/Script/human.gd
@@ -1,5 +1,50 @@
 extends Node2D
-const speed = 50
+var speed = 50
 var direction = 1
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+#for moving
 func _process(delta: float) -> void:
 	position.x += speed * delta * direction
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	if position.x < -550 or position.x > 600:
+		queue_free()
+		
+#for interacting
+var player_enter = false #check player enter the body
+var be_scared = false # ensure only change direction once
+@onready var coin_scene: PackedScene = preload("res://Scene/coin.tscn")
+var first = true 
+@onready var hurt_sounds: AudioStreamPlayer = $hurt
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	player_enter = true
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	player_enter = false
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact") and player_enter:
+		hurt_sounds.play()
+		if (!be_scared):
+			be_scared = true
+			direction = -1 * direction
+			speed = 150
+			animated_sprite_2d.play("runaway")
+		elif (first):
+			first = false
+			direction = -1 * direction
+			animated_sprite_2d.play("beat")
+			speed = 0
+			await animated_sprite_2d.animation_finished
+			#play hit animation
+			var new_coin = coin_scene.instantiate() # create new coin
+			new_coin.position = self.position + Vector2(direction * 30, -10) 
+			# create coin on the ground and at back of the human
+			get_node("/root/GameManager").add_child(new_coin) 
+			#create coin under gameManager to ensure the score is added
+			animated_sprite_2d.play("runaway")
+			direction = -1 * direction
+			speed = 300 #run away faster
+			
