fcfaf952c17b7129d5349151b83724cfbfb4561d
Will Zalmezak <wz59@illinois.edu>
2025-10-14 01:21:31 -0500

    Updated temp "main_menu" scene
    "main_menu" now has 5 buttons with temporary sprites that print to console upon being clicked

---
+++ cs-honors-proj/Scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+func _on_continue_button_pressed() -> void:
+	print("Loading save files!")
+	pass # Replace with function body.
+	
+func _on_new_game_button_pressed() -> void:
+	print("Creating new game!")
+	pass # Replace with function body.
+
+func _on_options_button_pressed() -> void:
+	print("Opening settings!")
+	pass # Replace with function body.
+
+func _on_tutorial_button_pressed() -> void:
+	print("Tutorializing!")
+	pass # Replace with function body.
+
+func _on_quit_button_pressed() -> void:
+	print("Quitting!")
+	pass # Replace with function body.
