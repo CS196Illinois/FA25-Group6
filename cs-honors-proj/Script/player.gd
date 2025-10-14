fc134255cef64d6913314961ce53a6b3a3d7a07b
qc33 <qc33@illinois.edu>
2025-10-15 05:32:18 +0800

    change beat animation

--- cs-honors-proj/Script/player.gd
+++ cs-honors-proj/Script/player.gd
@@ -5,7 +5,6 @@ const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
