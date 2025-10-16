93a7b449ba4c2a13f51f8669d163af6079160f82
jessicaliu6740 <jessicaliu6740@gmail.com>
2025-10-16 02:27:01 -0500

    tree collect acorn mini game UI and basic movement

--- cs-honors-proj/Scripts/squirrel.gd
+++ cs-honors-proj/Scripts/squirrel.gd
@@ -1,25 +1,43 @@
 extends CharacterBody2D
 
+const CLIMABLE_LAYER = "is_climable"
+const SPEED = 150.0
+const JUMP_VELOCITY = -250.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var on_climb: bool
+var climbing: bool
 
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	on_climb = true
+	
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	on_climb = false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
+	var grounded = is_on_floor()
+	var horizontal_dir = Input.get_axis("left", "right")
+	
+	if on_climb:
+		var vertical_dir = Input.get_axis("up", "down")
+		if vertical_dir:
+			velocity.y = vertical_dir * SPEED
+			climbing = not grounded
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+	# Apply gravity when jumping
+	elif not grounded:
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	
+	# Handle jump
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or climbing):
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	
+	# Movement input
+	if horizontal_dir:
+		velocity.x = horizontal_dir * SPEED
+		if on_climb: climbing = not grounded
+		else: climbing = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
