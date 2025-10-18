83a563903688d5f5ae5cb1e80985be21cd6a893a
Will Zalmezak <wz59@illinois.edu>
2025-10-18 00:00:47 -0500

    Added really rudimentary settings menu

--- cs-honors-proj/Scripts/main_menu.gd
+++ cs-honors-proj/Scripts/main_menu.gd
@@ -1,21 +1,22 @@
 extends Control
 
+func _ready() -> void:
+	$MarginContainer/VBoxContainer/MenuOptions/ContinueButton.grab_focus()
+
 func _on_continue_button_pressed() -> void:
+	# USE THIS COMAMND TO CHANGE SCENE
+	# get_tree().change_scene_to_file("NAME OF SCENE")
 	print("Loading save files!")
-	pass # Replace with function body.
 	
 func _on_new_game_button_pressed() -> void:
 	print("Creating new game!")
-	pass # Replace with function body.
 
 func _on_options_button_pressed() -> void:
-	print("Opening settings!")
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/options.tscn")
 
 func _on_tutorial_button_pressed() -> void:
+	# get_tree().change_scene_to_file("TUTORIAL SCENE")
 	print("Tutorializing!")
-	pass # Replace with function body.
 
 func _on_quit_button_pressed() -> void:
-	print("Quitting!")
-	pass # Replace with function body.
+	get_tree().quit()
