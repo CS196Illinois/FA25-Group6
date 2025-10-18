5eab19cfadf308d95aeb0cec9d5d10798cb78f14
qc33 <qc33@illinois.edu>
2025-10-19 02:41:43 +0800

    debugging (avoiding moving during interacting)

--- cs-honors-proj/Script/player.gd
+++ cs-honors-proj/Script/player.gd
@@ -23,15 +23,14 @@ func _physics_process(delta: float) -> void:
 	elif direction < 0:
 		animated_sprite_2d.flip_h = true
 	# play animations each time when press "K" without loop
-	if Input.is_action_pressed("interact") and is_on_floor():
-		if !playing:
-			animated_sprite_2d.play("beat")
-			await animated_sprite_2d.animation_finished
-			playing = true;
+	if Input.is_action_pressed("interact") and is_on_floor() and !playing:
+		animated_sprite_2d.play("beat")
+		await animated_sprite_2d.animation_finished
+		playing = true
 	elif Input.is_action_just_released("interact") and is_on_floor():
 		playing = false;
 	#static state, normal run and jump
-	if is_on_floor():
+	if is_on_floor() and !playing:
 		if direction == 0:
 			animated_sprite_2d.play("idle")
 		else:
@@ -39,9 +38,12 @@ func _physics_process(delta: float) -> void:
 	else:
 		animated_sprite_2d.play("jump")
 	#apply movement
-	if direction:
-		velocity.x = direction * SPEED
+	if !playing :
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = 0
+		#avoid moing when interact
 	move_and_slide()
