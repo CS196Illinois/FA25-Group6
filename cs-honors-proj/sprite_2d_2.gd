ade62000c69e1eaebcfce09db37ddb52dbf88a64
FlyingDolphin42 <fclatch@gmail.com>
2025-10-25 15:50:21 -0500

    Added parallaxing

---
+++ cs-honors-proj/sprite_2d_2.gd
@@ -0,0 +1,10 @@
+extends Sprite2D
+
+func _ready():
+	print("Starting...")
+	#print("Before:", position)
+	#position += Vector2(150, 20)
+	#print("After:", position)
+#func _on_timer_timeout():
+	print("Did it")
+	position += Vector2(1, 0)
