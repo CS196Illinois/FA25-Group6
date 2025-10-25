112ccc8be71432768c328e6264b4284c8081c949
jessicaliu6740 <jessicaliu6740@gmail.com>
2025-10-25 14:24:04 -0500

    update

--- cs-honors-proj/Scripts/enemy.gd
+++ cs-honors-proj/Scripts/enemy.gd
@@ -7,12 +7,15 @@ var direction = -1
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var ray_cast_right_down: RayCast2D = $RayCastRightDown
 @onready var ray_cast_left_down: RayCast2D = $RayCastLeftDown
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _process(delta):
 	if ray_cast_right.is_colliding() or not ray_cast_right_down.is_colliding():
 		direction = -1
+		animated_sprite.flip_h = true
 	if ray_cast_left.is_colliding() or not ray_cast_left_down.is_colliding():
 		direction = 1
+		animated_sprite.flip_h = false
 		
 	position.x += direction * SPEED * delta
 
--- cs-honors-proj/Scripts/squirrel.gd
+++ cs-honors-proj/Scripts/squirrel.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -250.0
 const ATTACK_VELOCITY = 10
 
 @onready var cooldown_timer = $Timer
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var on_climb: bool
 var climbing: bool
@@ -29,33 +30,51 @@ func _on_cooldown_timer_timeout():
 	
 func _physics_process(delta: float) -> void:
 	var grounded = is_on_floor()
+	
+# horizontal direction: -1, 0, 1
 	var horizontal_dir = Input.get_axis("left", "right")
+	# flip sprite based on left right
+	if (horizontal_dir > 0):
+		animated_sprite.flip_h = false
+	elif (horizontal_dir < 0):
+		animated_sprite.flip_h = true
+	# keep track of last moved direction
 	if (not horizontal_dir == 0.0): last_dir = horizontal_dir
 	
-	if on_climb:
-		var vertical_dir = Input.get_axis("up", "down")
-		if vertical_dir:
-			velocity.y = vertical_dir * SPEED
-			climbing = not grounded
-		else:
-			velocity.y = move_toward(velocity.y, 0, SPEED)
-	# Apply gravity when jumping
-	elif not grounded:
-		velocity += get_gravity() * delta
-	
-	# Handle jump
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or climbing):
-		velocity.y = JUMP_VELOCITY
-	
-	# Movement input
+# horizontal movement input
 	if horizontal_dir:
 		velocity.x = horizontal_dir * SPEED
 		if on_climb: climbing = not grounded
 		else: climbing = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+# horizontal movement animation
+	if (horizontal_dir == 0 && (grounded || climbing)):
+		animated_sprite.play("squirrel_idle")
+	elif (grounded || climbing):
+		animated_sprite.play("squirrel_run")
+	else:
+		animated_sprite.play("squirrel_jump")
+		
+# climbing movement up down
+	if on_climb:
+		# vertical direction: -1, 0, 1
+		var vertical_dir = Input.get_axis("up", "down")
+		# vertical climbing movement input
+		if vertical_dir:
+			velocity.y = vertical_dir * SPEED
+			climbing = not grounded
+		else:
+			velocity.y = move_toward(velocity.y, 0, SPEED)
+# Apply gravity when jumping
+	elif not grounded:
+		velocity += get_gravity() * delta
 	
-	# Attack input
+# Handle jump --> NEED TO FIX CLIMBING JUMP
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or climbing):
+		velocity.y = JUMP_VELOCITY
+	
+# attack input --> NEED TO FIX
 	if Input.is_action_just_pressed("attack"):
 		if can_attack:
 			can_attack = false
