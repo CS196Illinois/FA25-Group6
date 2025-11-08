4aa67fa344adc995657928fa4ee643cf2a4ef59a
ahuang1027 <surferalex9000@gmail.com>
2025-11-08 00:29:29 -0600

    hunger bar

---
+++ cs-honors-proj/Bars/needs.gd
@@ -0,0 +1,9 @@
+extends Node
+
+class_name Need
+var value : float
+@export var max_value : float
+@export var start_value : float
+@export var regen_rate : float
+@export var decay_rate : float
+@export var UI_bar : Node
