7e0bf089ac37128bc07c0a2fe7fe8c36bb55db4d
FlyingDolphin42 <fclatch@gmail.com>
2025-11-15 12:51:12 -0600

    Update player.gd

--- cs-honors-proj/Script/player.gd
+++ cs-honors-proj/Script/player.gd
@@ -7,9 +7,24 @@ const JUMP_VELOCITY = -300.0
 var playing = false
 var birth_position: Vector2
 
+var strength = 10
+
+# Double Jump Mechanic
+var double_jump_unlocked = true
+var max_jumps = 1
+var current_jumps = 0
+
+
+# Charged Jump Mechanic
+const MAX_CHARGE_TIME = 1.5 # seconds
+var charging = false
+var charge_time = 0.0
+
 func _ready():
 	birth_position = position
 	#get birth position for killzone
+	if double_jump_unlocked:
+		max_jumps = 2
 func respawn():
 	position = birth_position
 	velocity = Vector2.ZERO
@@ -18,10 +33,12 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	else:
+		current_jumps = 0
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() || current_jumps < max_jumps):
 		velocity.y = JUMP_VELOCITY
+		current_jumps += 1
 	# Get the input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
 	# flip the sprite
