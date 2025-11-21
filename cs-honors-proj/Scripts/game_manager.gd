7880c5b90de5c02f218ab32c000ce7501c8cc807
qc33 <qc33@illinois.edu>
2025-11-21 08:19:00 +0800

    human creating and beginning dialog coming back
    Co-Authored-By: nd <38202+nd@users.noreply.github.com>

--- cs-honors-proj/Scripts/game_manager.gd
+++ cs-honors-proj/Scripts/game_manager.gd
@@ -5,12 +5,12 @@ extends Node
 var score = 0
 var first_reach_amount = true;
 #prevent repeated dialogue
-func _ready():
+func start_game():
 	#connect autoload GameManager to find ScoreLabel
 	score_label = get_tree().get_root().find_child("ScoreLabel", true, false)
 	#connect dialogic signal and begining initial dialog
 	Dialogic.signal_event.connect(_on_dialogic_signal)
-	# Dialogic.start("beginning")
+	Dialogic.start("beginning")
 
 #count coins
 func add_point():
@@ -31,28 +31,29 @@ func _on_dialogic_signal(argument: String):
 @export var create_y := 80   # create on ground
 
 var time_count = 0.0
-#func create_human():
-	#var human = human_copy.instantiate() # create new human scene
-	#var random_side = randi() % 2 # create random 0 or 1, allows npc appear randomely in left or right
-	#if random_side == 0 :
-		##set original place and direction
-		#human.position = Vector2(left_create_x, create_y);
-		#human.direction = 1;
-	#else:
-		#human.position = Vector2(right_create_x, create_y);
-		#human.direction = -1;
-	#add_child(human) 
+func create_human():
+	var human = human_copy.instantiate() # create new human scene
+	var random_side = randi() % 2 # create random 0 or 1, allows npc appear randomely in left or right
+	if random_side == 0 :
+		#set original place and direction
+		human.position = Vector2(left_create_x, create_y);
+		human.direction = 1;
+	else:
+		human.position = Vector2(right_create_x, create_y);
+		human.direction = -1;
+	add_child(human) 
 	#add our new human
 
 #creat human each 3 seconds
-#func _process(delta):
-	#time_count += delta
-	#if time_count >= creat_cd:
-		#time_count = 0
-		#create_human()
+func human_creating(delta):
+	time_count += delta
+	if time_count >= creat_cd:
+		time_count = 0
+		create_human()
 		
 #add conditional dialog
-	#if score == 10 and first_reach_amount:
-		#first_reach_amount = false
-		#Dialogic.start("coins collection")
+func _process(delta: float) -> void:
+	if score == 10 and first_reach_amount:
+		first_reach_amount = false
+		Dialogic.start("coins collection")
 		#
