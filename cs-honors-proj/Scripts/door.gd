d45d9fac4ef9fbc5f4867fa6525fdc2c9cae74b5
qc33 <qc33@illinois.edu>
2025-12-07 04:19:54 +0800

    trying fixing door again

--- cs-honors-proj/Scripts/door.gd
+++ cs-honors-proj/Scripts/door.gd
@@ -10,6 +10,7 @@ class_name Door
 
 
 func _on_body_entered(body: Node2D) -> void:
+	print("enter")
 	if body is Player:
 		print("into")
 		NavigationManager.go_to_level(destination_level_tag, destination_door_tag)
