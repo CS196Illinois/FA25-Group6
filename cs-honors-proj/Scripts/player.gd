87805026354af51f5ae124ea835f312af1ad462a
qc33 <qc33@illinois.edu>
2025-12-07 03:56:33 +0800

    fix dialog bug and add changing scene

--- cs-honors-proj/Scripts/player.gd
+++ cs-honors-proj/Scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+class_name Player
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
@@ -18,7 +19,12 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	#avoid moving during the dialog
+	if GameManager.is_in_dialogue:
+		playing = false
+		velocity.x = 0
+		move_and_slide()
+		return
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
