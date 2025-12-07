6b9d806e617deb934a3b6a479844635d63650a3d
qc33 <qc33@illinois.edu>
2025-12-07 15:06:52 +0800

    fix translating bug

--- cs-honors-proj/Scripts/left.gd
+++ cs-honors-proj/Scripts/left.gd
@@ -7,4 +7,5 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _process(delta):
 	if player_in_area:
+		GlobalState.last_scene_name = ""
 		get_tree().change_scene_to_file("res://Scenes/games.tscn")
